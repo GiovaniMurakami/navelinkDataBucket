bbf20cf4014c9c12fe3da7e12e5a968063bba10d
Giovani Murakami <giovani.murakami@outlook.com>
2024-04-01 21:46:42 -0300

    feat: added encrypted data

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,5 +1,6 @@
 const Data = require("../models/Data");
 const User = require("../models/User");
+const { encrypt, decrypt } = require("../helpers/cryto");
 
 module.exports = class DashboardController {
     static async home(req, res) {
@@ -9,7 +10,15 @@ module.exports = class DashboardController {
             include: Data,
             plain: true,
         });
+
         const data = user.Data.map((r) => r.dataValues);
+        data.map(function (r) {
+            r.username = decrypt(JSON.parse(r.username));
+            r.password = decrypt(JSON.parse(r.password));
+            r.link = decrypt(JSON.parse(r.link));
+            r.description = decrypt(JSON.parse(r.description));
+        });
+
         res.render("dashboard/home", { data: data });
     }
     static create(req, res) {
@@ -18,18 +27,17 @@ module.exports = class DashboardController {
     static async createPost(req, res) {
         const { username, password, link, description } = req.body;
         const userId = req.session.userid;
-        /*Encriptação*/
 
-        /*/ Encriptação */
-        const data = {
-            username,
-            password,
-            link,
-            description,
+        const encryptedData = {
+            username: JSON.stringify(encrypt(username)),
+            password: JSON.stringify(encrypt(password)),
+            link: JSON.stringify(encrypt(link)),
+            description: JSON.stringify(encrypt(description)),
             UserId: userId,
         };
+
         try {
-            await Data.create(data);
+            await Data.create(encryptedData);
             req.flash("message", "Dado armazenado com sucesso!");
             DashboardController.home(req, res);
         } catch (e) {
@@ -57,25 +65,40 @@ module.exports = class DashboardController {
                 DashboardController.home(req, res);
                 return;
             }
-            if (data.UserId != req.session.userid) {
+
+            console.log(data.dataValues);
+
+            const decryptedData = {
+                id: data.id,
+                username: decrypt(JSON.parse(data.username)),
+                password: decrypt(JSON.parse(data.password)),
+                link: decrypt(JSON.parse(data.link)),
+                description: decrypt(JSON.parse(data.description)),
+                createdAt: data.createdAt,
+                updateAt: data.updateAt,
+                UserId: data.UserId,
+            };
+
+            if (decryptedData.UserId != req.session.userid) {
                 req.flash("message", "Erro, escolha um dado que te pertence");
                 DashboardController.home(req, res);
                 return;
             }
-            console.log(data.dataValue);
-            res.render("dashboard/visualize", { data: data.dataValues });
+
+            res.render("dashboard/visualize", { data: decryptedData });
         } catch (e) {
             console.log(e);
         }
     }
+
     static async editData(req, res) {
         const id = req.body.id;
         const { username, password, link, description } = req.body;
         const newData = {
-            username,
-            password,
-            link,
-            description,
+            username: JSON.stringify(encrypt(username)),
+            password: JSON.stringify(encrypt(password)),
+            link: JSON.stringify(encrypt(link)),
+            description: JSON.stringify(encrypt(description)),
         };
         try {
             await Data.update(newData, { where: { id: id } });
